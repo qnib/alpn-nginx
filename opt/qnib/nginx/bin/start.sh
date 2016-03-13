20bbaedff7268b3a82ce3736d14582b5f279ff44
Christian Kniep <christian@qnib.org>
2016-03-13 17:52:30 +0100

    add dummywebsites

--- opt/qnib/nginx/bin/start.sh
+++ opt/qnib/nginx/bin/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+## Create dummy websites
+consul-template -consul localhost:8500 -once -template "/etc/consul-templates/nginx/http.html.ctmpl:/usr/share/nginx/html/index.html"
+consul-template -consul localhost:8500 -once -template "/etc/consul-templates/nginx/https.html.ctmpl:/usr/share/nginx/htmls/index.html"
+
 /usr/sbin/nginx
 
