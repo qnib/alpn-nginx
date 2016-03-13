4cec795092c7591064556da45e25319235820672
Christian Kniep <christian@qnib.org>
2016-03-13 15:57:47 +0100

    complete stack for test

--- test/run.sh
+++ test/run.sh
@@ -2,15 +2,17 @@
 
 set -e
 
+export TEST_CONTAINER=$(echo nginx_test_$(date +%s))
+
 echo "## start stack"
 docker-compose up -d
 
 echo "#### Check HTTP connection"
 echo ">> docker exec -t test curl -I  http://nginx.node.consul"
-docker exec -t test curl -I -k http://nginx.node.consul
+docker exec -t ${TEST_CONTAINER} curl -I -k http://nginx.node.consul
 echo "#### Check HTTPS connection"
 echo ">> docker exec -t test curl -Ivk https://nginx.node.consul"
-docker exec -t test curl -Ivk https://nginx.node.consul
+docker exec -t ${TEST_CONTAINER} curl -Ivk https://nginx.node.consul
 
 echo "## remove stack"
 docker-compose kill 
