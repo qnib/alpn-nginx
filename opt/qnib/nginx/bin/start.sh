6a53d9f558cd9b6bda40ede38d8336478c356819
Christian Kniep <christian@qnib.org>
2016-07-19 12:53:22 +0200

    let the default.conf be derived from a template

--- opt/qnib/nginx/bin/start.sh
+++ opt/qnib/nginx/bin/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+## Create default.config
+consul-template -once -template "/etc/consul-templates/nginx/default.conf.ctmpl:/etc/nginx/conf.d/default.conf"
 ## Create dummy websites
 consul-template -consul localhost:8500 -once -template "/etc/consul-templates/nginx/http.html.ctmpl:/usr/share/nginx/html/index.html"
 consul-template -consul localhost:8500 -once -template "/etc/consul-templates/nginx/https.html.ctmpl:/usr/share/nginx/htmls/index.html"
